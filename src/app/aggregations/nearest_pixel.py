10fd740c3ec652877c26f46a738a37492615fe73
phurtac <pitehrh@gmail.com>
2023-05-28 17:42:55 -0400

    added: initial state of aggregation process

---
+++ src/app/aggregations/nearest_pixel.py
@@ -0,0 +1,11 @@
+"""Proccess of Aggregation of pixels"""
+import geopandas as gdp
+import numpy as np
+import pandas as pd
+
+
+class Meta_Pixel:
+    """Class of aggregated pixels"""
+
+    def __init__(self) -> None:
+        pass
